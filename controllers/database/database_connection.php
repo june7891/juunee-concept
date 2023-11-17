48199342c58d5c5373c76d686e8df37835ed0fe3
Toma Juneviciute <97102025+june7891@users.noreply.github.com>
2023-11-17 18:51:01 +0100

    Update database_connection.php

--- controllers/database/database_connection.php
+++ controllers/database/database_connection.php
@@ -3,7 +3,7 @@ abstract class Database_connection {
     private static $pdo;
 
     private static function setConnexion(){
-        // self::$pdo = new PDO('mysql:host=tomajuy1987.mysql.db;dbname=tomajuy1987;charset=utf8', 'tomajuy1987', 'Zaibaket2018');
+
         self::$pdo = new PDO('mysql:host=localhost;dbname=juunee;charset=utf8', 'root', '');
         self::$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
     }
